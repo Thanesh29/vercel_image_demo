3beed6cf61ee17f85be9fa539bdf08246d9cca6b
Thanesh29 <thaneshrao414@gmail.com>
2025-01-20 00:12:51 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -14,18 +14,9 @@ export default function Home() {
         {/* Example Image Without Size and Lazy Loading */}
         <div>
           <Image 
-            src="/path/to/your/image.jpg" 
+            src="/test_image.jpg" 
             alt="Sample Image"
             // Notice there's no width and height specified here
-          />
-        </div>
-        
-        {/* Other images could be added in a similar manner */}
-        <div>
-          <Image 
-            src="/path/to/another-image.jpg" 
-            alt="Another Image"
-            // No size or lazy loading attributes here either
           />
         </div>
       </main>
