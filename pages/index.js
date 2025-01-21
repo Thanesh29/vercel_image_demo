647659f052a2e10f5b0af6e428c3f3ef60c0a536
Thanesh29 <thaneshrao414@gmail.com>
2025-01-21 08:54:27 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,15 @@
+import Image from 'next/image';
+
 export default function Home() {
   return (
     <div style={{ textAlign: 'center' }}>
       <h1>Optimized Image Showcase</h1>
-      <p>Leveraging the <code>&lt;img /&gt;</code> Component</p>
-      <img
+      <p>Leveraging the <code>&lt;Image /&gt;</code> Component</p>
+      <Image
         src="/test_image.jpg"
         alt="Test Image"
-        width="600"
-        height="400"
+        width={600}
+        height={400}
       />
     </div>
   );
